35e1d24012d8ab3d8a08f2d810fdcffaae57a56b
akinov <ayumoto10@gmail.com>
2025-03-23 18:55:11 +0900

    fix cline rules

--- src/services/cline.ts
+++ src/services/cline.ts
@@ -2,7 +2,7 @@ import path from 'path';
 import { BaseService } from './base';
 
 export class ClineService extends BaseService {
-  constructor(targetExtension: string = 'mdc') {
+  constructor(targetExtension: string = 'md') {
     super(
       'cline',
       path.join(process.cwd(), '.cline', 'rules'),
